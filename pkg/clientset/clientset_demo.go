ea734ed30bf46f815ee3eefc746c078b6f4685bd
qintao <670026955@qq.com>
2024-08-16 20:19:48 +0800

    k8s informer list and watch

--- pkg/clientset/clientset_demo.go
+++ pkg/clientset/clientset_demo.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -23,9 +24,33 @@ func main() {
 		panic(err)
 	}
 
-	deployments, err := clientset.AppsV1().Deployments("default").List(context.TODO(), metav1.ListOptions{})
-	for _, deployment := range deployments.Items {
-		fmt.Println(deployment.Name)
-	}
+	//deployments, err := clientset.AppsV1().Deployments("default").List(context.TODO(), metav1.ListOptions{})
+	//for _, deployment := range deployments.Items {
+	//	fmt.Println(deployment.Name)
+	//}
+
+	//factory := informers.NewSharedInformerFactory(clientset, 0)
+	factory := informers.NewSharedInformerFactoryWithOptions(clientset, 0, informers.WithNamespace("default"))
+	informer := factory.Core().V1().Pods().Informer()
+
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			pod := obj.(*v1.Pod)
+			fmt.Println("add event: ", pod.Name)
+		},
+		UpdateFunc: func(oldObj, newObj interface{}) {
+			old := oldObj.(*v1.Pod)
+			newpod := newObj.(*v1.Pod)
+			fmt.Println("update event: ", old.Name, "  --> ", newpod.Name)
+		},
+		DeleteFunc: func(obj interface{}) {
+			pod := obj.(*v1.Pod)
+			fmt.Println("delete event:", pod.Name)
+		},
+	})
+	stopch := make(chan struct{})
+	factory.Start(stopch)
+	factory.WaitForCacheSync(stopch)
+	<-stopch
 
 }
